8826d12a17b68b8b7bf2a795c6ec72ba6b8b0deb
Amanda McNary <amcnary.su@gmail.com>
2020-05-27 23:52:32 -0700

    Create basic React app using parcel

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootEl = document.getElementById('root');
+
+ReactDOM.render(
+  <p>Hello</p>,
+  document.getElementById('root'),
+);
